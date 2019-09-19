f48919f4c0700f88829093843d9e91e5057f8dff
IRSAGE <sibo.egide98@gmail.com>
2019-09-19 15:51:49 +0200

    chore(folder-structure):setting a folder structure -adding a folder structure -adding some images that will be used in the web
    finishes #168640643

---
+++ UI/js/script.js
@@ -0,0 +1,37 @@
+//navigations
+const openNav = () => {
+  document.getElementById('mySidenav').style.width = '250px';
+};
+
+const closeNav = () => {
+  document.getElementById('mySidenav').style.width = '0';
+};
+//slide javascript
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 8000); 
+}
+  //modal
+  // Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
